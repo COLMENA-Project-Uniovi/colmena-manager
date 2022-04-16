3886fb6c9b327ac12e98511eb42780c9e6a4163b
Borja <borjarodrilore@gmail.com>
2022-04-16 12:23:05 +0200

    Changed name and developed Subjects model

---
+++ plugins/Colmena/AcademicalManager/src/Controller/SubjectsController.php
@@ -0,0 +1,193 @@
+<?php
+
+namespace Colmena\SubjectsManager\Controller;
+
+use Colmena\SubjectsManager\Controller\AppController;
+use Cake\Event\Event;
+use App\Encryption\EncryptTrait;
+use Cake\Http\Exception\ForbiddenException;
+use Cake\Http\Exception\UnauthorizedException;
+use Cake\ORM\TableRegistry;
+
+class SubjectsController extends AppController
+{
+    use EncryptTrait;
+
+    public $entity_name = 'asignatura';
+    public $entity_name_plural = 'asignaturas';
+
+    // Default pagination settings
+    public $paginate = [
+        'limit' => 20,
+        'order' => [
+            'User.id' => 'ASC'
+        ]
+    ];
+
+    protected $table_buttons = [
+        'Editar' => [
+            'icon' => '<i class="fas fa-edit"></i>',
+            'url' => [
+                'controller' => 'Subjects',
+                'action' => 'edit',
+                'plugin' => 'Colmena/SubjectsManager'
+            ],
+            'options' => [
+                'class' => 'green-icon',
+                'escape' => false
+            ]
+        ],
+        'Borrar' => [
+            'icon' => '<i class="fas fa-trash-alt"></i>',
+            'url' => [
+                'controller' => 'Subjects',
+                'action' => 'delete',
+                'plugin' => 'Colmena/SubjectsManager'
+            ],
+            'options' => [
+                'confirm' => '¿Está seguro de que desea eliminar la asignatura?',
+                'class' => 'red-icon',
+                'escape' => false
+            ]
+        ]
+    ];
+
+    protected $header_actions = [
+        'Añadir asignatura' => [
+            'url' => [
+                'controller' => 'Subjects',
+                'plugin' => 'Colmena/SubjectsManager',
+                'action' => 'add'
+            ]
+        ],
+    ];
+
+    protected $tab_actions = [];
+
+    /**
+     * Before filter
+     *
+     * @param \Cake\Event\Event $event The beforeFilter event.
+     *
+     */
+    public function beforeFilter(\Cake\Event\EventInterface $event)
+    {
+        parent::beforeFilter($event);
+        $this->Auth->allow([
+            'login','register'
+        ]);
+    }
+
+    /**
+     * Index method
+     *
+     * @return void
+     */
+    public function index($keyword = null)
+    {
+        if ($this->request->is('post')) {
+            //recover the keyword
+            $keyword = $this->request->getData('keyword');
+            //re-send to the same controller with the keyword
+            return $this->redirect(['action' => 'index', $keyword]);
+        }
+
+        // Paginator
+        $settings = $this->paginate;
+        // If performing search, there is a keyword
+        if ($keyword != null) {
+            // Change pagination conditions for searching
+            $settings['conditions'] = [
+                'OR' => [
+                    $this->getName() . '.name LIKE' => '%' . $keyword . '%',
+                ]
+            ];
+        }
+
+        //prepare the pagination
+        $this->paginate = $settings;
+        $entities = $this->paginate($this->modelClass);
+
+        $this->set('header_actions', $this->getHeaderActions());
+        $this->set('table_buttons', $this->getTableButtons());
+        $this->set('entities', $entities);
+        $this->set('_serialize', 'entities');
+        $this->set('keyword', $keyword);
+    }
+
+    /**
+     * Add method
+     *
+     * @return void Redirects on successful add, renders view otherwise.
+     */
+    public function add()
+    {
+        $entity = $this->{$this->getName()}->newEmptyEntity();
+        if ($this->request->is('post')) {
+            $entity = $this->{$this->getName()}->patchEntity($entity, $this->request->getData());
+
+            if ($this->{$this->getName()}->save($entity)) {
+                $this->Flash->success('la asignatura se ha guardado correctamente.');
+                return $this->redirect(['action' => 'edit', $entity->id]);
+            } else {
+                $error_msg = '<p>La asignatura no se ha guardado correctamente. Por favor, revisa los datos e inténtalo de nuevo.</p>';
+                foreach ($entity->errors() as $field => $error) {
+                    $error_msg .= '<p>' . $error['message'] . '</p>';
+                }
+                $this->Flash->error($error_msg, ['escape' => false]);
+            }
+        }
+
+        $this->set(compact('entity', 'roles'));
+    }
+
+    /**
+     * Edit method
+     *
+     * @param string|null $id Product id.
+     * @return void Redirects on successful edit, renders view otherwise.
+     * @throws \Cake\Http\Exception\NotFoundException When record not found.
+     */
+    public function edit($id = null, $locale = null)
+    {
+        $this->setLocale($locale);
+        $entity = $this->{$this->getName()}->get($id);
+
+        if ($this->request->is(['patch', 'post', 'put'])) {
+            $entity = $this->{$this->getName()}->patchEntity($entity, $this->request->getData());
+
+            if ($this->{$this->getName()}->save($entity)) {
+                $this->Flash->success('la asignatura se ha guardado correctamente.');
+                return $this->redirect(['action' => 'edit', $entity->id, $locale]);
+            } else {
+                $error_msg = '<p>La asignatura no se ha guardado correctamente. Por favor, revisa los datos e inténtalo de nuevo.</p>';
+                foreach ($entity->errors() as $field => $error) {
+                    $error_msg .= '<p>' . $error['message'] . '</p>';
+                }
+                $this->Flash->error($error_msg, ['escape' => false]);
+            }
+        }
+
+        $this->set('tab_actions', $this->getTabActions('Users', 'edit', $entity));
+        $this->set(compact('entity', 'roles'));
+    }
+
+    /**
+     * Delete method
+     *
+     * @param string|null $id Article id.
+     * @return \Cake\Network\Response|null Redirects to index.
+     * @throws \Cake\Http\Exception\NotFoundException When record not found.
+     */
+    public function delete($id = null)
+    {
+        $this->request->allowMethod(['post', 'delete']);
+        $entity = $this->{$this->getName()}->get($id);
+        if ($this->{$this->getName()}->delete($entity)) {
+            $this->Flash->success('la asignatura se ha borrado correctamente.');
+        } else {
+            $this->Flash->error('la asignatura no se ha borrado correctamente. Por favor, inténtalo de nuevo más tarde.');
+        }
+        return $this->redirect(['action' => 'index']);
+    }
+}
---
+++ plugins/Colmena/AcademicalManager/src/Plugin.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Colmena\SubjectsManager;
+
+use Cake\Core\BasePlugin;
+
+/**
+ * Plugin for Colemena\SubjectsManager
+ */
+class Plugin extends BasePlugin
+{
+}
